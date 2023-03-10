e3879db0d0550192784cbdc607df792614dddecb
Sehii Propolov <job.tenshi@gmail.com>
2023-03-10 20:42:06 +0200

    made selectable characters clickable

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -1,7 +1,6 @@
 import error from '../errorMessage/error.gif';
 
 const ErrorMessage = () => {
-
     return (
         <img src={error} style={{margin: '0 auto', width: 'auto', height: '250px'}} alt="error404" />
     )
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -39,7 +39,8 @@ class RandomChar extends Component {
     updateChar = () => {
         this.setState({
             loading: true
-        })
+        });
+
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
 
         this.marvelService
